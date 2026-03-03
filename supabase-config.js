a3b9808cf82bf3264fa4367d22236629821a1de7
valdy247 <valdymena2020@gmail.com>
2026-03-02 23:57:42 -0500

    chore: set Supabase project url and anon key

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
+  url: "https://zvlpejdmkgltgvxpiggl.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2bHBlamRta2dsdGd2eHBpZ2dsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MDk5NjEsImV4cCI6MjA4ODA4NTk2MX0.jAYWNzB09eNIh7NsWJi-1IFzF_oIHl_NawAe609zqAM",
   table: "site_content",
   rowId: 1
 };
